8f628af45e50452bbb0dd378c946c1f011bc14a0
Elvis Farias <elvisrfarias1@gmail.com>
2024-09-30 14:25:36 -0300

    Segundo commit

---
+++ Tarefa.cs
@@ -0,0 +1,11 @@
+﻿namespace GerenciadorTarefas;
+
+public class Tarefa
+{
+    public int IdTarefa { get; set; }
+    public required string Nome { get; set; }
+    public string TarefaDescricao { get; set; } = string.Empty;
+    public string Prioridade { get; set; } = string.Empty; // Você pode definir se tem prioridade alta, média ou baixa, por exemplo;
+    public DateTime? DeadLine { get; set; } = DateTime.Now;
+    public bool Status { get; set; }  // Você pode definir três status: concluída, em andamento e aguardando, por exemplo.
+}
